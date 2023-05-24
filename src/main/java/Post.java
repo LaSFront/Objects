bef8d98d604de4e4b158ec382d1c063e2db19f0e
“Larisa” <lsergeevna1978@mail.ru>
2023-05-24 20:49:15 +0400

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name;
+    public String pasport;
+    public String patronimic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+    public FormDate birthday;
+}
